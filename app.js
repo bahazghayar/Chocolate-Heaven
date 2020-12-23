7b4a92ead2e24c75dbb40e6610d84269a1097cea
bahazghayar <bahazghayar@hotmail.com>
2020-12-23 15:39:32 +0200

    JS code added

---
+++ app.js
@@ -0,0 +1,12 @@
+
+alert('Welcome Happiness Seeker') 
+var chocolate_kind
+var time = prompt('You will get a white chocolate as A GIFT if the sun has not yet set. otherwise, you will get a dark chocolate, your health is important. So what is the time? ' ) ;
+
+if (time <= 5 ) {
+    chocolate_kind= 'White Chocolate' ;
+} else {
+    chocolate_kind= 'Dark Chocolate' ; 
+}
+ 
+alert (`${chocolate_kind}, lucky you `); 
