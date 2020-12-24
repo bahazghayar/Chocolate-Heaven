dc8d4bf121aff1f27b45eb8c04b59ed1c4edc300
bahazghayar <bahazghayar@hotmail.com>
2020-12-24 16:04:06 +0200

    Lab8 added

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 
 alert('Welcome Happiness Seeker') 
-var chocolate_kind
-var time = prompt('You will get a white chocolate as A GIFT if the sun has not yet set. otherwise, you will get a dark chocolate, your health is important. So what is the time? ' ) ;
+
+
+var chocolate_kind ; 
+var time ;
+ 
+while ( isNaN(time) || time === '' ) {
+
+    time = prompt('You will get a white chocolate as A GIFT if the sun has not yet set. otherwise, you will get a dark chocolate, your health is important. So what is the time? ' ) ;
+    
+}
 
 if (time <= 5 ) {
     chocolate_kind= 'White Chocolate' ;
@@ -10,3 +18,13 @@ if (time <= 5 ) {
 }
  
 alert (`${chocolate_kind}, lucky you `); 
+
+
+var Number_of_chocolates ; 
+Number_of_chocolates = prompt ('How many chocolates do you want?') ; 
+for (i=0  ; i< Number_of_chocolates  ; i++   ) {
+    document.write( '<img src="2be5d40218.jpg"> ')
+
+    
+    
+}
